dfff606730d513345e2f231def194dcd5cdec0c1
Vinayak Suryavanshi <95699405+vbs30@users.noreply.github.com>
2024-09-01 13:38:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,20 +12,18 @@ $(document).ready(function(){
     $('.minus-btn').on('click', function() {
         var $quantityInput = $(this).siblings('.quantity-input');
         var currentVal = parseInt($quantityInput.val());
-        if (!isNaN(currentVal)) {
-            $quantityInput.val(currentVal - 1);
-        } else {
-            $quantityInput.val(1);
+        if (!isNaN(currentVal) && currentVal > 1) {
+            $quantityInput.val(currentVal - 1); // Decrease value but not below 1
         }
     });
 
     $('.plus-btn').on('click', function() {
         var $quantityInput = $(this).siblings('.quantity-input');
         var currentVal = parseInt($quantityInput.val());
-        if (!isNaN(currentVal) && currentVal > 1) {
-            $quantityInput.val(currentVal + 1);
+        if (!isNaN(currentVal)) {
+            $quantityInput.val(currentVal + 1); // Increase value
         } else {
-            $quantityInput.val(1);
+            $quantityInput.val(1); // If input is invalid, reset to 1
         }
     });
 });
